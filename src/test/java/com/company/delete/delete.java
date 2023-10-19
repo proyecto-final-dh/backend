5638fbb72e5fa037d70ef32ab707e9ba187058c1
Ezequiel Safdie <ezesafdie@gmail.com>
2023-10-19 17:48:02 -0300

    Test commit to try pr

---
+++ src/test/java/com/company/delete/delete.java
@@ -0,0 +1,12 @@
+package com.company.delete;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class delete {
+
+    @Test
+    public void pass() {
+        Assertions.assertEquals(1, 1);
+    }
+}
