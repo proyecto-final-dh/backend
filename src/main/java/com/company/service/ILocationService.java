4983b49caa9b29d025fd91771c9e69aa01d7c1bb
Nat <nahiara@gmail.com>
2023-10-26 21:04:32 -0300

    #11 Add feature Locations (#12)
    * feat: add Location model, service and repository
    
    * feat: add Location service interface and controller
    
    * feat: add location tests
    
    * fix: edit imports in LocationController
    
    * fix: edit imports in Location model
    
    * fix: edit imports
    
    * fix: delete duplicated dependencies tag
    
    * fix: change location id data type for Long to int
    
    * Updated tests to use static assertion methods
    
    ---------
    
    Co-authored-by: Ezequiel Safdie <ezesafdie@gmail.com>

---
+++ src/main/java/com/company/service/ILocationService.java
@@ -0,0 +1,14 @@
+package com.company.service;
+
+import com.company.model.entity.Location;
+
+import java.util.List;
+
+public interface ILocationService {
+    List<Location> findAll();
+    Location findById(int id) throws Exception;
+    Location findByCountryAndCityAndState(String country, String city, String state);
+    Location update(int id, Location location) throws Exception;
+    Location save(Location location) throws Exception;
+    void deleteById(int id) throws Exception;
+}
---
+++ src/main/java/com/company/service/LocationService.java
@@ -0,0 +1,82 @@
+package com.company.service;
+
+import com.company.model.entity.Location;
+import com.company.repository.LocationRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@AllArgsConstructor
+@Service
+public class LocationService implements ILocationService{
+
+
+    private final LocationRepository locationRepository;
+
+    public List<Location> findAll(){
+        List<Location> locations = locationRepository.findAll();
+        return locations;
+    }
+
+    public Location findById(int id) throws Exception {
+        try {
+            Optional<Location> location = locationRepository.findById(id);
+            if (location.isPresent()) {
+                return location.get();
+            } else {
+                throw new Exception("Location with id " + id + " not found.");
+            }
+        } catch (Exception e) {
+            throw new Exception("Location with id " + id + " not found.");
+        }
+    }
+
+    public Location findByCountryAndCityAndState(String country, String city, String state){
+        return locationRepository.findByCountryAndCityAndState(country, city, state);
+    }
+
+       public Location update(int id, Location location) throws Exception{
+        Optional<Location> exists = locationRepository.findById(id);
+        try {
+            if(exists.isPresent()) {
+                {
+                    location.setId(id);
+                    return locationRepository.save(location);
+                }
+            } else {
+                throw new Exception("ID missing");
+            }
+        } catch (Exception e) {
+            throw new Exception("Location with id " + id + " not found.");
+        }
+    }
+
+    public Location save(Location location) throws Exception {
+
+        Location ifExistsLocation = locationRepository.findByCountryAndCityAndState(location.getCountry(), location.getCity(), location.getState());
+        if (ifExistsLocation == null) {
+            return locationRepository.save(location);
+        } else {
+            throw new Exception("That location already exist");
+        }
+    }
+
+
+    public void deleteById(int id) throws Exception {
+        Optional<Location> exists = locationRepository.findById(id);
+        try {
+            if(exists.isPresent()) {
+                {
+                    locationRepository.deleteById(id);
+                }
+            } else {
+                throw new Exception("ID missing");
+            }
+        } catch (Exception e) {
+            throw new Exception("Location with id " + id + " not found.");
+        }
+    }
+
+
+}
