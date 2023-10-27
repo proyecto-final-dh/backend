8751ebba6aaf5073d12587123cb758fbc404b2b3
Sofia Munoz <sofia.munoz@valtech.com>
2023-10-26 21:51:59 -0300

    Delete endpoint for breeds

--- src/test/java/com/company/BreedsTest/BreedsTest.java
+++ src/test/java/com/company/BreedsTest/BreedsTest.java
@@ -1,10 +1,14 @@
 package com.company.BreedsTest;
 
 import com.company.controller.BreedsController;
+import com.company.controller.SpeciesController;
 import com.company.model.entity.Breeds;
 import com.company.model.entity.Species;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
@@ -17,37 +21,56 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.notNull;
 
 @SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class BreedsTest {
     @Autowired
     private BreedsController breedsController;
-    @BeforeEach
-    public void setup() {}
-
-    @Test
-    public void testGetAllSpecies() {
-        List<Breeds> speciesList = breedsController.getAllSpecies();
-        assertTrue(speciesList.size() != 0);
+    @Autowired
+    private SpeciesController speciesController;
+    private static Breeds newBreeds;
+    @BeforeAll
+    public void setup() {
+        // Configurar datos de prueba
+        int speciesID = 1;
+        Species speciesId1 = (Species) speciesController.getSpeciesById(speciesID).getBody();
+        newBreeds = new Breeds("prueba1", speciesId1);
     }
-
+    @AfterAll
+    public void teardown() {
+        newBreeds = null;
+    }
     @Test
-    public void testGetSpeciesById() {
+    public void testGetAllBreeds() {
+        List<Breeds> breedsList = breedsController.getAllBreeds();
+        assertTrue(breedsList.size() != 0);
+    }
+    @Test
+    public void testGetBreedsById() {
         int id = 1;
-        ResponseEntity<Object> result = breedsController.getSpeciesById(id);
+        ResponseEntity<Object> result = breedsController.getBreedsById(id);
         assertEquals(HttpStatus.OK, result.getStatusCode());
         notNull(result.getBody().getClass());
         assertTrue(result.getBody() instanceof Breeds);
         assertEquals(((Breeds) result.getBody()).getId(), 1);
     }
-
     @Test
-    public void testCreateSpecies() {
-
-        Species species = new Species(1,"gato");
-        Breeds newBreeds = new Breeds("prueba1", species);
+    public void testCreateBreeds() {
         ResponseEntity<Object> result = breedsController.createBreeds(newBreeds);
-
         assertEquals(HttpStatus.CREATED, result.getStatusCode());
-        assertEquals(newBreeds, result.getBody());
-        //TODO: Borrar registro de la BD
+        //assertEquals(newBreeds, result.getBody());
+
+        int breedId = ((Breeds) result.getBody()).getId();
+        ResponseEntity<Object> resultDelete = breedsController.deleteBreeds(breedId);
+        assertEquals(HttpStatus.NO_CONTENT, resultDelete.getStatusCode());
+    }
+    @Test
+    public void testDeleteBreeds() {
+
+        ResponseEntity<Object> result = breedsController.createBreeds(newBreeds);
+        var bodyResult = ((Breeds) result.getBody());
+        int breedId = bodyResult.getId();
+
+        ResponseEntity<Object> resultDelete = breedsController.deleteBreeds(breedId);
+        assertEquals(HttpStatus.NO_CONTENT, resultDelete.getStatusCode());
     }
 }
