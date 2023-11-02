7f8ec2a7fb714b08a09642bccda664a19031db0a
javtr <javtriana.z@gmail.com>
2023-11-01 21:19:01 -0500

    history update

--- src/main/java/com/company/controller/HistoryController.java
+++ src/main/java/com/company/controller/HistoryController.java
@@ -1,7 +1,7 @@
 package com.company.controller;
 
-import com.company.model.entity.Stories;
-import com.company.service.StoriesService;
+import com.company.model.entity.History;
+import com.company.service.HistoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,34 +18,34 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/stories")
-public class StoriesController {
-    private final StoriesService storiesService;
+@RequestMapping("/history")
+public class HistoryController {
+    private final HistoryService historyService;
 
     @Autowired
-    public StoriesController(StoriesService storiesService) {
-        this.storiesService = storiesService;
+    public HistoryController(HistoryService historyService) {
+        this.historyService = historyService;
     }
 
     @PostMapping
-    public ResponseEntity<Object> createStory(@RequestBody Stories story) {
+    public ResponseEntity<Object> createHistory(@RequestBody History story) {
         try {
-            Stories newStory = storiesService.createStories(story);
+            History newStory = historyService.createHistory(story);
             return new ResponseEntity<>(newStory, HttpStatus.CREATED);
         } catch (ResponseStatusException ex) {
             throw new ResponseStatusException(
                     HttpStatus.BAD_REQUEST, ex.getMessage());
         } catch (Exception ex) {
             throw new ResponseStatusException(
-                    HttpStatus.INTERNAL_SERVER_ERROR, "Error while creating the story");
+                    HttpStatus.INTERNAL_SERVER_ERROR, "Error while creating the history");
         }
     }
 
 
     @GetMapping
-    public List<Stories> getAllStories() {
+    public List<History> getAllHistory() {
         try {
-            return storiesService.getAllStories();
+            return historyService.getAllHistory();
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
         }
@@ -52,32 +53,48 @@ public class StoriesController {
 
 
     @GetMapping("/{id}")
-    public ResponseEntity<Object> getStoryById(@PathVariable int id) {
+    public ResponseEntity<Object> getHistoryById(@PathVariable int id) {
         try {
-            Stories story = storiesService.getStoryById(id);
+            History story = historyService.getHistoryById(id);
             return ResponseEntity.ok(story);
         } catch (ResponseStatusException ex) {
             throw new ResponseStatusException(
                     HttpStatus.NOT_FOUND, ex.getMessage(), ex);
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while fetching the story");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while fetching the history");
         }
     }
 
 
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Object> deleteStory(@PathVariable int id) {
+    public ResponseEntity<Object> deleteHistory(@PathVariable int id) {
         try {
-            storiesService.deleteStory(id);
+            historyService.deleteHistory(id);
             return ResponseEntity.noContent().build();
         } catch (ResponseStatusException ex) {
             throw new ResponseStatusException(
                     HttpStatus.NOT_FOUND, ex.getMessage(), ex);
         } catch (Exception ex) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while deleting the story");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while deleting the history");
         }
     }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateHistory(@PathVariable int id, @RequestBody History history) {
+        try {
+            History updatedHistory = historyService.updateHistory(id, history);
+            return ResponseEntity.ok(updatedHistory);
+        } catch (ResponseStatusException ex) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, ex.getMessage(), ex);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while updating the history");
+        }
+    }
+
+
+
 
 
 }
