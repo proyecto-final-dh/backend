eafe36a2902db7583016882fc5801088c8fe070b
Ezequiel Safdie <ezesafdie@gmail.com>
2023-11-12 22:07:11 -0300

    Added name and id to status, and created methods to validate and find status

--- src/main/java/com/company/enums/PetStatus.java
+++ src/main/java/com/company/enums/PetStatus.java
@@ -1,7 +1,29 @@
 package com.company.enums;
 
+import lombok.Getter;
+
+import java.util.Arrays;
+
 public enum PetStatus {
-    MASCOTA_PROPIA,
-    EN_ADOPCION,
-    ADOPTADA
+    MASCOTA_PROPIA("MASCOTA_PROPIA", "Mascota propia"),
+    EN_ADOPCION("EN_ADOPCION", "En adopción"),
+    ADOPTADA("ADOPTADA", "Adoptada");
+
+    PetStatus(String id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    @Getter
+    private String id;
+    @Getter
+    private String name;
+
+    public static boolean isValidStatus(String status) {
+        return Arrays.stream(PetStatus.values()).anyMatch(e -> e.getId().equalsIgnoreCase(status));
+    }
+
+    public static PetStatus getStatusById(String status) {
+        return Arrays.stream(PetStatus.values()).filter(e -> e.getId().equalsIgnoreCase(status)).findFirst().get();
+    }
 }
