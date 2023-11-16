ddf3df4abce0d3dd837986d111f8600c8a33c1c5
Natalia Garcia <nahiara@gmail.com>
2023-11-16 00:20:34 -0300

    feat: add pet gender enum validation

--- src/main/java/com/company/enums/PetGender.java
+++ src/main/java/com/company/enums/PetGender.java
@@ -1,6 +1,18 @@
 package com.company.enums;
 
+import java.util.Arrays;
+
 public enum PetGender {
-    MACHO,
-    HEMBRA
+    MACHO("MACHO"),
+    HEMBRA("HEMBRA");
+
+    PetGender(String id) {
+        this.id = id;
+    }
+
+    private String id;
+
+    public static boolean isValidGender(String gender) {
+        return Arrays.stream(PetStatus.values()).anyMatch(e -> e.getId().equalsIgnoreCase(gender));
+    }
 }
