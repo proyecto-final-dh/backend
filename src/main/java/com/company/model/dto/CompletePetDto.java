b8e69f9c692dc1e880d7235282f7032aa1b38325
Sofia Munoz <sofia.munoz@valtech.com>
2023-11-19 21:35:21 -0300

    Deleting innecesary imports

--- src/main/java/com/company/model/dto/CompletePetDto.java
+++ src/main/java/com/company/model/dto/CompletePetDto.java
@@ -3,16 +3,10 @@ package com.company.model.dto;
 import com.company.enums.PetStatus;
 import com.company.model.entity.Breeds;
 import com.company.model.entity.UserDetails;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.antlr.v4.runtime.misc.NotNull;
 
 import java.util.List;
 @Getter
