af6e711e09802babc9d0819da57694a7f4b59702
StefanySB <stefany9209@gmail.com>
2023-11-28 10:25:06 -0500

    se configuran los endpoints que no necesita auntentuicacion

--- src/main/java/com/company/config/security/SecurityConfiguration.java
+++ src/main/java/com/company/config/security/SecurityConfiguration.java
@@ -5,14 +5,8 @@ import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
-import org.apache.hc.client5.http.io.HttpClientConnectionManager;
 import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
 import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
-import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactoryBuilder;
-import org.apache.hc.core5.http.ClassicHttpResponse;
-import org.apache.hc.core5.http.io.entity.EntityUtils;
-import org.apache.hc.core5.http.ssl.TLS;
-import org.apache.hc.core5.ssl.SSLContextBuilder;
 import org.apache.hc.core5.ssl.SSLContexts;
 import org.apache.hc.core5.ssl.TrustStrategy;
 import org.springframework.beans.factory.annotation.Value;
@@ -27,6 +21,7 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
 import javax.net.ssl.SSLContext;
@@ -35,13 +30,12 @@ import javax.net.ssl.SSLContext;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
-import java.security.cert.X509Certificate;
-import java.util.Arrays;
 
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebMvc
+@EnableGlobalMethodSecurity(prePostEnabled = false, securedEnabled = false, jsr250Enabled = true)
 public class SecurityConfiguration {
 
 
@@ -49,35 +43,27 @@ public class SecurityConfiguration {
     private String jwkSetUri;
 
 
-@Bean
-public JwtDecoder jwtDecoder() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
-    RestTemplate restTemplate = restTemplate();
+    @Bean
+    public JwtDecoder jwtDecoder() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+        RestTemplate restTemplate = restTemplate();
         NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withJwkSetUri(this.jwkSetUri).restOperations(restTemplate).build();
         // Configura el resto del JwtDecoder como lo necesites
         return jwtDecoder;
     }
 
 
-
     public RestTemplate restTemplate() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
         final TrustStrategy acceptingTrustStrategy = (cert, authType) -> true;
 
-        SSLContext sslContext = SSLContexts.custom()
-                .loadTrustMaterial(null, acceptingTrustStrategy)
-                .build();
+        SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
 
         SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
 
-        PoolingHttpClientConnectionManager connectionManager = PoolingHttpClientConnectionManagerBuilder.create()
-                .setSSLSocketFactory(csf)
-                .build();
+        PoolingHttpClientConnectionManager connectionManager = PoolingHttpClientConnectionManagerBuilder.create().setSSLSocketFactory(csf).build();
 
-        CloseableHttpClient httpClient = HttpClients.custom()
-                .setConnectionManager( connectionManager)
-                .build();
+        CloseableHttpClient httpClient = HttpClients.custom().setConnectionManager(connectionManager).build();
 
-        HttpComponentsClientHttpRequestFactory requestFactory =
-                new HttpComponentsClientHttpRequestFactory();
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
 
         requestFactory.setHttpClient(httpClient);
 
@@ -86,40 +72,28 @@ public JwtDecoder jwtDecoder() throws KeyStoreException, NoSuchAlgorithmExceptio
     }
 
 
-
-
-
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.
-                oauth2ResourceServer(oauth2ResourceServer ->
-                        oauth2ResourceServer.jwt(jwt ->
-                                {
-                                    try {
-                                        jwt.decoder(jwtDecoder())
-                                                .jwtAuthenticationConverter( new JwtAuthenticationConverter());
-                                    } catch (KeyStoreException e) {
-                                        throw new RuntimeException(e);
-                                    } catch (NoSuchAlgorithmException e) {
-                                        throw new RuntimeException(e);
-                                    } catch (KeyManagementException e) {
-                                        throw new RuntimeException(e);
-                                    }
-                                }
-                        )
-                );
+        http.oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(jwt -> {
+            try {
+                jwt.decoder(jwtDecoder()).jwtAuthenticationConverter(new JwtAuthenticationConverter());
+            } catch (KeyStoreException e) {
+                throw new RuntimeException(e);
+            } catch (NoSuchAlgorithmException e) {
+                throw new RuntimeException(e);
+            } catch (KeyManagementException e) {
+                throw new RuntimeException(e);
+            }
+        }));
 
-        http
-                .cors(cors -> cors.disable())
+        http.cors(cors -> cors.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeRequests(authz -> authz
+                        .requestMatchers("/locations/**","/species/**","/breeds/**","/pets/**").permitAll()
 
 
-                .sessionManagement((session) -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
-                .authorizeRequests()
-                .anyRequest().authenticated();
+                        .anyRequest().authenticated());
         return http.build();
     }
 
