2f42bae889dc14aadf1b607997adf73c8f0a09db
StefanySB <stefany9209@gmail.com>
2023-11-30 22:26:46 -0500

    Primera revision

---
+++ src/main/java/com/company/controller/UserController.java
@@ -0,0 +1,59 @@
+package com.company.controller;
+
+
+import com.company.exceptions.ResourceNotFoundException;
+import com.company.repository.UserKeycloakRepository;
+import com.company.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private UserKeycloakRepository userKeycloakRepository;
+    private UserService userService;
+
+    public UserController(UserKeycloakRepository userKeycloakRepository, UserService userService) {
+        this.userKeycloakRepository = userKeycloakRepository;
+        this.userService = userService;
+    }
+
+
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<?> findUserById(@PathVariable String userId) throws ResourceNotFoundException {
+        return ResponseEntity.ok().body(userService.findById(userId));
+    }
+
+    @GetMapping
+    public ResponseEntity<?> findUserById() throws ResourceNotFoundException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userId = "";
+
+        if (authentication instanceof JwtAuthenticationToken jwtAuthToken) {
+            userId = jwtAuthToken.getToken().getClaims().get("sub").toString();
+        }
+        return ResponseEntity.ok().body(userService.findById(userId));
+    }
+
+
+
+    /*
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserKeycloak> buscarid(@PathVariable String id) throws ResourceNotFoundException {
+        return  ResponseEntity.ok().body(userKeycloakRepository.findUser(id));
+
+    }
+
+     */
+
+
+}
--- src/main/java/com/company/repository/UserKeycloakRepository.java
+++ src/main/java/com/company/repository/UserKeycloakRepository.java
@@ -1,11 +1,13 @@
 package com.company.repository;
 
+import com.company.exceptions.ResourceNotFoundException;
 import com.company.model.entity.UserKeycloak;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
+import javax.ws.rs.NotFoundException;
 import java.util.List;
 
 @Repository
@@ -19,16 +21,25 @@ public class UserKeycloakRepository {
         this.keycloakClient = keycloakClient;
     }
 
-    public UserKeycloak findUser(String keycloakUserId){
-        UserRepresentation usersRepresentation=keycloakClient.realm(realm).users().get(keycloakUserId).toRepresentation();
+    public UserKeycloak findUser(String keycloakUserId) throws ResourceNotFoundException {
+
+        UserRepresentation usersRepresentation = null;
         UserKeycloak userResponse = new UserKeycloak();
-        userResponse.setUserName(usersRepresentation.getUsername());
-        userResponse.setEmail(usersRepresentation.getEmail());
-        userResponse.setName(usersRepresentation.getFirstName());
-        userResponse.setLastname(usersRepresentation.getLastName());
-        userResponse.setIdUsuario(keycloakUserId);
+        try {
+            usersRepresentation = keycloakClient.realm(realm).users().get(keycloakUserId).toRepresentation();
+            userResponse = new UserKeycloak();
+            userResponse.setUserName(usersRepresentation.getUsername());
+            userResponse.setEmail(usersRepresentation.getEmail());
+            userResponse.setName(usersRepresentation.getFirstName());
+            userResponse.setLastname(usersRepresentation.getLastName());
+            userResponse.setIdUsuario(keycloakUserId);
+        } catch (NotFoundException e) {
+            throw new ResourceNotFoundException("User does not exits in Keycloak");
+
+        }
 
         return userResponse;
-    }
 
+
+    }
 }
