4d4366e16daf7c09395c972d5a3ee7bdc3152092
Ezequiel Safdie <69767459+ezesafdie@users.noreply.github.com>
2023-12-05 13:49:45 -0300

    New cors configuration (#68)

--- src/main/java/com/company/ProyectoFinalApplication.java
+++ src/main/java/com/company/ProyectoFinalApplication.java
@@ -9,16 +9,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class ProyectoFinalApplication {
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
-			}
-		};
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoFinalApplication.class, args);
 	}
